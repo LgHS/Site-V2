98ae8e86392b1f97e70ce0e5aa9b30e21279736e
Dicatoro <mattimmer@gmail.com>
2016-04-07 03:16:17 +0200

    Added feature to show if the hackerspace is open or not.

--- lghs_website/__init__.py
+++ lghs_website/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template
 from .views import general
 
@@ -5,8 +6,24 @@ app = Flask(__name__)
 app.config.from_object('lghs_website.default_config')
 app.config.from_envvar('LGHS_WEBSITE_CONFIG', silent=True)
 
+
 @app.errorhandler(404)
 def not_found(error):
 	return render_template('404.html'), 404
+
+
+@app.context_processor
+def is_open():
+	open_hours = (
+        # day_of_week, start_hour, end_hour
+		(2, 19, 23),
+		(6, 13, 18),
+	)
+	now = datetime.now()
+	for day, start_hour, end_hour in open_hours:
+		if day == now.weekday() and start_hour <= now.hour <= end_hour:
+			return {"is_open" : True}
+	return {"is_open": False}
+
 
 app.register_blueprint(general.mod)
