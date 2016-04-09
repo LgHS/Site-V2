504bf88ce8464b9e717fa91394f1648f16f8739b
JeromeJ <e-warning@hotmail.com>
2016-04-09 23:32:31 +0200

    Documentend hs_is_open + HTML refactoring

--- lghs_website/__init__.py
+++ lghs_website/__init__.py
@@ -20,13 +20,32 @@ def not_found(error):
 	return render_template('404.html'), 404
 
 
-def hs_is_open(time=None):
-	"""Indicates if the HS is opened at said time. time defaults to: datetime.now()"""
+def hs_is_open(time:datetime=None, opening_hours:dict=None) -> bool:
+	"""Indicate if the HS is opened at said time. Only relies on hours.
+	
+	Default values:
+		time = datetime.now()
+		opening_hours = app.config['OPENING_HOURS']
+	
+	
+	If we provide a day when the HS is open
+	>>> hs_is_open(datetime(2016, 1, 6, 15, 0), {'wednesday': (13, 18)})
+	True
+	>>> hs_is_open(datetime(2016, 1, 6, 12, 30), {'wednesday': (13, 18)})
+	False
+	
+	If provided with a day when the HS doesn't open at all.
+	>>> hs_is_open(datetime.datetime(2016, 1, 7, 15, 30), {'wednesday': (13, 18)})
+	False
+	"""
 	
 	if time is None:
 		time = datetime.now()
 	
-	start_hour, end_hour = app.config['OPENING_HOURS'].get(
+	if opening_hours is None:
+		opening_hours = app.config['OPENING_HOURS']
+	
+	start_hour, end_hour = opening_hours.get(
 		weekday_name[time.weekday()],  # Retrieve opening hours for said day
 		(0, 0)  # will always invalidate if the HS doesn't open on said day
 	)
