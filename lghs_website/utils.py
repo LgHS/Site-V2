56f57adaecc871348fcdcf7ff95902aaf6e7688d
JeromeJ <e-warning@hotmail.com>
2016-05-13 04:49:18 +0200

    Refactoring lghs_website.utils.weekday_name .

--- lghs_website/utils.py
+++ lghs_website/utils.py
@@ -1,6 +1,5 @@
-# Note: Better than relying on strftime which relies on local lang
-weekday_int = {
-	'monday': 0, 'tuesday': 1, 'wednesday': 2,
-	'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6
-}
-weekday_name = {v:k for k,v in weekday_int.items()}
+# Note: We're not using strftime's weekday because it relies on local lang.
+weekday_name = (
+	'monday', 'tuesday', 'wednesday',
+	'thursday', 'friday', 'saturday', 'sunday'
+)
